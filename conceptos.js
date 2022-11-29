1ae1e339e638e2eb8f1411a9607f98adc4762e0a
Cranpa <cris11242000.cp@gmail.com>
2022-11-28 22:19:02 -0500

    Se ingresan ciclos de practicas en conceptos

---
+++ conceptos.js
@@ -0,0 +1,26 @@
+// Expresion <---  retornan un valor
+// Statement <--- Realizan una accion
+
+
+function suma(params){
+    return  
+}
+for(var i = 1; i < 10; i++){
+    console.log('Hola');
+    continue
+    console.log('Nunca entra');
+
+}
+var valor = 'C'
+switch (valor) {
+    case 'A':
+        console.log('Si entra ');
+        breBak;
+    case 'b':
+
+        break;
+
+    default:
+        console.log('Siempre entra');
+        break;
+}
